fe34472440c4f050334d40582822a6aff24fd371
Matheus Deon Bordignon <matheusdb_@outlook.com>
2021-07-27 23:32:52 -0300

    delete folders

--- frontend/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://pure-caverns-68092.herokuapp.com/api",
-  headers: {'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'}
-});
-
-export default api;
